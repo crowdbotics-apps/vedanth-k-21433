417fef129ccb26a38eeb3479b761d4d9623cf24f
Crowdbotics <team@crowdbotics.com>
2020-10-13 13:20:08 +0000

    Update models

--- backend/vedanth_k_21433/wsgi.py
+++ backend/vedanth_k_21433/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vedanth_k_21433.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vedanth_k_21433.settings")
 
 application = get_wsgi_application()
